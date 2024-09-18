963812ddf0695ce71a0496b25956693b22c19145
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2024-09-18 17:17:33 -0300

    main

--- src/componentes/personagens/ListaPersonagens.js
+++ src/componentes/personagens/ListaPersonagens.js
@@ -4,12 +4,26 @@ import styles from "./ListaPersonagens.module.css";
 
 import CardPersonagem from "./CardPersonagem";
 
+import { MdArrowLeft, MdArrowRight } from "react-icons/md";
+
+
+
 
 function ListaPersonagens() {
     const [paginaAtual,setPaginaAtual] = useState(1)
+    const [totalDePaginas,setTotalDePaginas] = useState()
     const [dadosParaCards,setDadosParaCards]=useState()
 
- 
+    useEffect(()=>{
+        fetch('https://dragonball-api.com/api/characters')
+        .then(response => response.json())
+        .then(data => {
+            const totalItems = data.meta.totalItems;
+            setTotalDePaginas(Math.ceil(totalItems/12))
+        })
+        .catch(error => console.error('Erro ao obter personagens:', error));
+
+    },[])
 
     useEffect(()=>{
       
@@ -26,15 +40,29 @@ function ListaPersonagens() {
                 console.error('Erro ao buscar dados:', error);
             })
         
-    },[])
+    },[paginaAtual])
     
    
     
     return (
-        <div className={styles.caixaDaLista}>
-            {dadosParaCards ? dadosParaCards.items.map(c=>(
-                <CardPersonagem key={c.id} nome={c.name} raca={c.race} identificacao={c.id} imagem={c.image} />
-            )) : ""}
+        <div>
+            <div className={styles.caixaDaLista}>
+                {dadosParaCards ? dadosParaCards.items.map(c=>(
+                    <CardPersonagem key={c.id} nome={c.name} raca={c.race} identificacao={c.id} imagem={c.image} />
+                )) : ""}
+            </div>
+            <div className={styles.passarPaginas}>
+                <MdArrowLeft className={styles.setas}/>
+                
+                {[...Array(totalDePaginas)].map((_,index)=>{
+                        return <span key={index+1}>{index+1}</span>
+                })}
+              
+               
+                
+                <MdArrowRight className={styles.setas}/>
+                
+            </div>
         </div>
     )
 }
